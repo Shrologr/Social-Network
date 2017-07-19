60f247474a4005ff386e1f481467d1039b70a90a
Shrologr <smakarchuk111@gmail.com>
2017-07-19 18:06:18 +0300

    Added some stuff to services

--- Social_Network/Social_Network.BLL/Services/UserPhotoService.cs
+++ Social_Network/Social_Network.BLL/Services/UserPhotoService.cs
@@ -14,5 +14,55 @@ namespace Social_Network.BLL.Services
 {
     public class UserPhotoService:IUserPhotoService
     {
+        private IUnitOfWork Database;
+        public UserPhotoService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+        public IEnumerable<UserPhotosDTO> GetAllPhotos()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<UserPhotos, UserPhotosDTO>());
+            return Mapper.Map<IEnumerable<UserPhotos>, IEnumerable<UserPhotosDTO>>(Database.UserPhotos.GetAll());
+        }
+
+        public IEnumerable<UserPhotosDTO> GetUserPhotos(int? id)
+        {
+            if (id == null)
+            {
+                throw new ValidationException("Id is null", "");
+            }
+            Mapper.Initialize(cfg => cfg.CreateMap<UserPhotos, UserPhotosDTO>());
+            return Mapper.Map<IEnumerable<UserPhotos>, IEnumerable<UserPhotosDTO>>(Database.UserPhotos.Find(s=>s.User_ID == id.Value));
+        }
+
+        public UserPhotosDTO GetPhoto(int? id)
+        {
+            if (id == null)
+            {
+                throw new ValidationException("Id is null", "");
+            }
+            var photo = Database.UserPhotos.Get(id.Value);
+            if (photo == null)
+            {
+                throw new ValidationException("No photo with this id", "");
+            }
+            Mapper.Initialize(cfg => cfg.CreateMap<UserPhotos, UserPhotosDTO>());
+            return Mapper.Map<UserPhotos, UserPhotosDTO>(photo);
+        }
+
+        public void CreatePhoto(UserPhotosDTO photo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePhoto(UserPhotosDTO photo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
     }
 }
