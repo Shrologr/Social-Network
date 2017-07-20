3552987718aed0a0db8c1f85c23b1f2f9a03c497
Shrologr <smakarchuk111@gmail.com>
2017-07-20 15:50:57 +0300

    Started unit testing

---
+++ Social_Network/Social_Network.Tests/BusinessLayerTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Social_Network.BLL.DTO;
+using Social_Network.BLL.Infrastructure;
+using Social_Network.BLL.Interfaces;
+using Social_Network.BLL.Services;
+using Social_Network.DAL.EF;
+using Social_Network.DAL.Implementation;
+using Social_Network.DAL.Interfaces;
+
+
+namespace Social_Network.Tests
+{
+    [TestClass]
+    public class BusinessLayerTest
+    {
+        [TestMethod]
+        public void UserCreateTest()
+        {
+            try
+            {
+                using (IUnitOfWork uow = new EFUnitOfWork("name=SocialNetworkDatabaseContext"))
+                {
+                    Console.WriteLine("Test start...");
+                    NetworkUsers user = new NetworkUsers()
+                    {
+                        Name = "Ivan",
+                        Surname = "Ivanov",
+                        Birthday = new DateTime(1994, 12, 3),
+                        Gender = "Male",
+                        Mail = "",
+                        URL = "",
+                        Photo_ID = 0,
+                        User_Password = ""
+                    };
+                    uow.NetworkUsers.Create(user);
+                    Console.WriteLine("User created...");
+                    uow.Save();
+                    Console.WriteLine("User saved...");
+                    Assert.IsNotNull(uow.NetworkUsers.Find(s => s.Name == "Ivan"));
+
+                    uow.Dispose();
+                }
+            }
+            catch(Exception ex)
+            {
+                System.IO.File.WriteAllText("file.txt",ex.Message);
+                Console.WriteLine(ex.Message);
+                Assert.Fail();
+            }
+        }
+    }
+}
